23fe000d6ad9c3517219f55db4996021f61f00c3
PoznanaKing <90681508+PoznanaKing@users.noreply.github.com>
2023-01-30 09:08:06 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,142 +1,56 @@
 ﻿using System;
+using System.IO;
 
-namespace ConsoleApp2
+namespace html_generator
 {
     class Program
     {
-        static bool Van_e_szam(string password)
-        {
-        
-        bool s = false;
-        string chs = "0123456789";
-        int i = 0;
-        while ((!s)&&(i<chs.Length))
-            {
-                if (password.IndexOf(chs[i])>=0)
-                {
-                    s = true;
-                }
-                i++;
-            }
-            return (s);
-            
-        }
-        static bool Van_e_kisbetu(string password)
-        {
-            bool s = false;
-            string chs = "öüóqwertzuiopőúasdfghjkléáűíyxcvbnm";
-            int i = 0;
-            while ((!s) && (i < chs.Length))
-            {
-                if (password.IndexOf(chs[i]) >= 0)
-                {
-                    s = true;
-                }
-                i++;
-            }
-            return (s);
-
-        }
-        static bool Van_e_nagybetu(string password)
-        {
-            bool s = false;
-            string chs = "ÖÜÓQWERTZUIOPŐÚASDFGHJKLÉÁŰÍYXCVBNM";
-            int i = 0;
-            while ((!s) && (i < chs.Length))
-            {
-                if (password.IndexOf(chs[i]) >= 0)
-                {
-                    s = true;
-                }
-                i++;
-            }
-            return (s);
-
-        }
-        static double mer (int hossz,string password)
-        {
-            DateTime a =DateTime.Now;
-            Van_e_kisbetu(password);
-            Van_e_nagybetu(password);
-            Van_e_szam(password);
-            DateTime b = DateTime.Now;
-
-            TimeSpan c = b - a;
-            return (c.TotalSeconds);
-        }
-        static double mer1(int hossz, string password)
-        {
-            DateTime a = DateTime.Now;
-            Van_e_kisbetu(password);
-            Van_e_nagybetu(password);
-            Van_e_szam(password);
-            DateTime b = DateTime.Now;
-
-            TimeSpan c = b - a;
-            return (c.TotalMinutes);
-        }
-        static double mer2(int hossz, string password)
-        {
-            DateTime a = DateTime.Now;
-            Van_e_kisbetu(password);
-            Van_e_nagybetu(password);
-            Van_e_szam(password);
-            DateTime b = DateTime.Now;
-
-            TimeSpan c = b - a;
-            return (c.TotalHours);
-        }
-        static double mer3(int hossz, string password)
-        {
-            DateTime a = DateTime.Now;
-            Van_e_kisbetu(password);
-            Van_e_nagybetu(password);
-            Van_e_szam(password);
-            DateTime b = DateTime.Now;
-
-            TimeSpan c = b - a;
-            return (c.TotalDays);
-        }
-        static double mer4(int hossz, string password)
-        {
-            DateTime a = DateTime.Now;
-            Van_e_kisbetu(password);
-            Van_e_nagybetu(password);
-            Van_e_szam(password);
-            DateTime b = DateTime.Now;
-
-            TimeSpan c = b - a;
-            return (c.TotalDays*365);
-        }
         static void Main(string[] args)
+        
         {
-            Console.WriteLine("A jelszó: ");
-            string password = Console.ReadLine();
-            Console.WriteLine($"Van-e számjegy: {Van_e_szam(password)}\nVan-e kisbetű: {Van_e_kisbetu(password)}\nVan-e nagybetu: {Van_e_nagybetu(password)}");
+            StreamWriter sw1 = new StreamWriter(@"C:\Users\Pozsgain\Desktop\Új mappa\style.css");
 
-            int hossz = password.Length;
-            long s = 0;
-            if (Van_e_szam(password))
-            {
-                s += 10;
-            }
-            if (Van_e_kisbetu(password))
-            {
-                s += 35;
-            }
-            if (Van_e_nagybetu(password))
-            {
-                s += 35;
-            }
-            
-            
-            double muveletek = Math.Pow(s, hossz);
+            sw1.WriteLine("table{\nborder-radius: 10px;\nborder-style: double;\nbackground-color: red;}");
+            sw1.WriteLine("th{\nborder-style:groove;\nborder-radius: 1px;}");
+            sw1.WriteLine("p{\ncolor: white}");
 
-            Console.WriteLine($"Az időtényező: {(mer(hossz, password) * s) *muveletek/1} Másodperc");
-            Console.WriteLine($"Az időtényező: {(mer1(hossz, password) * s) * muveletek / 1000} Perc");
-            Console.WriteLine($"Az időtényező: {(mer2(hossz, password) * s) * muveletek / 10000} Óra");
-            Console.WriteLine($"Az időtényező: {(mer3(hossz, password) * s) * muveletek / 100000} Nap");
-            Console.WriteLine($"Az időtényező: {(mer4(hossz, password) * s) * muveletek / 1000000} Év");
+            sw1.Close();
+
+            StreamWriter sw = new StreamWriter(@"C:\Users\Pozsgain\Desktop\Új mappa\index.html");
+            sw.WriteLine("<!DOCTYPE html>");
+            sw.WriteLine("<html lang="+'"'+"hu"+'"'+">");
+            sw.WriteLine("<head>");
+            sw.WriteLine("   <meta charset=" + '"' + "UTF - 8" + '"' + ">");
+            sw.WriteLine("<meta http-equiv="+'"'+"X - UA - Compatible"+'"'+" content="+'"'+"IE = edge"+'"'+">");
+            sw.WriteLine("<meta name = "+'"'+"viewport"+'"'+" content = "+'"'+"width = device - width, initial - scale = 1.0"+'"'+">");
+            sw.WriteLine("<title>Document</title>");
+            sw.WriteLine("<link rel="+'"'+"stylesheet"+'"'+" href="+'"'+"./style.css"+'"'+">");
+            sw.WriteLine("</head>");
+
+
+            sw.WriteLine("<body>");
+
+            sw.WriteLine("<table>");
+            for (int i = 0; i < 10; i++)
+            {
+                sw.WriteLine("<tr>");
+                sw.WriteLine("<th>\n<p>" + i * 1 + "</p>\n</th>\n");
+                sw.WriteLine("<th>\n<p>" + i * 2 + "</p>\n</th>\n");
+                sw.WriteLine("<th>\n<p>" + i * 3 + "</p>\n</th>\n");
+                sw.WriteLine("<th>\n<p>" + i * 4 + "</p>\n</th>\n");
+                sw.WriteLine("<th>\n<p>" + i * 5 + "</p>\n</th>\n");
+                sw.WriteLine("<th>\n<p>" + i * 6 + "</p>\n</th>\n");
+                sw.WriteLine("<th>\n<p>" + i * 7 + "</p>\n</th>\n");
+                sw.WriteLine("<th>\n<p>" + i * 8 + "</p>\n</th>\n");
+                sw.WriteLine("<th>\n<p>" + i * 9 + "</p>\n</th>\n");
+                sw.WriteLine("<th>\n<p>" + i * 10 + "</p>\n</th>\n");
+                sw.WriteLine("</tr>\n");
+            }
+            sw.WriteLine("</table>");
+
+
+            sw.WriteLine("</body>");
+            sw.Close();
         }
     }
 }
